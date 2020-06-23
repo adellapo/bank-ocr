41b9c792f0bdb1c2856422ce23579ca5a679c74f
Andres Della Porta <andres.m.dellaporta@gmail.com>
2020-06-23 12:29:12 -0300

    Se agrego funcionalidad de output

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -1,6 +1,8 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,15 +11,13 @@ public class Aplicacion {
 	public static void main(String[] args) throws IOException {
 
 		FileInputStream fis = new FileInputStream("input.txt");
+		FileOutputStream fos = new FileOutputStream("output.txt");
 
 		Lector lector = new Lector();
-
 		ArrayList<String> cuentas = lector.leer(fis);
 
-		cuentas.forEach(c -> {
-			System.out.println("CUENTA: [" + c + "]");
-		});
-
+		guardarCuentas(fos, cuentas);
+		
 		cuentas.forEach(c -> chequearSuma(c));
 	}
 
@@ -42,5 +42,35 @@ public class Aplicacion {
 
 			return 0;
 		}
+	}
+	
+	public static void guardarCuentas(FileOutputStream fos, ArrayList<String>cuentas) {
+		try {
+			
+			cuentas.forEach(c -> {
+				c.chars().forEach(chr -> {
+
+					try {
+						fos.write((char) chr);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				});
+				
+				try {
+					fos.write(13);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("CUENTA: [" + c + "]");
+			});
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
