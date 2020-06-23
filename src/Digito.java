16c5456232360d00c45cd47854588705905e504a
Andres Della Porta <andres.m.dellaporta@gmail.com>
2020-06-22 21:22:56 -0300

    terminada la User Story 1

--- src/Digito.java
+++ src/Digito.java
@@ -1,28 +1,66 @@
 
 public class Digito {
 
-	int MAXFILAS = 4;
-	int MAXCOLUMNAS = 3;
-	char digito[][] = new char[4][3];
+	private int MAXFILAS = 4;
+	private int MAXCOLUMNAS = 3;
+	private char digito[][] = new char[4][3];
 
-	public Digito() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public Digito(String line) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public void insertarCaracter(char caracter, int x, int y) {
-		this.digito[x][y] = caracter == ' ' ? '.' : caracter;
+	public void insertarCaracter(char caracter, int fila, int columna) {
+		this.digito[fila][columna] = caracter;
 	}
 
 	public void imprimirDigito() {
 		for (int i = 0; i < MAXFILAS; i++) {
 			for (int j = 0; j < MAXCOLUMNAS; j++) {
-				System.out.print((this.digito[i][j] == 0 ? '.' : this.digito[i][j] ));
+				System.out.print(this.digito[i][j]);
 			}
 			System.out.println();
 		}
+	}
+
+	public int decodificar() {
+
+		int nro = 0;
+		String digitoPlano = "";
+
+		digitoPlano = String.copyValueOf(digito[0]) + String.copyValueOf(digito[1]) + String.copyValueOf(digito[2]);
+
+		switch (digitoPlano) {
+		case "     |  |":
+			nro = 1;
+			break;
+		case " _  _||_ ":
+			nro = 2;
+			break;
+		case " _  _| _|":
+			nro = 3;
+			break;
+		case "   |_|  |":
+			nro = 4;
+			break;
+		case " _ |_  _|":
+			nro = 5;
+			break;
+		case " _ |_ |_|":
+			nro = 6;
+			break;
+		case " _   |  |":
+			nro = 7;
+			break;
+		case " _ |_||_|":
+			nro = 8;
+			break;
+		case " _ |_| _|":
+			nro =9;
+			break;
+		case " _ | ||_|":
+			nro = 0;
+			break;
+		default:
+			nro = -1;
+		}
+
+		return nro;
+
 	}
 }
